8ef8448d595f3f3c695924a3b707b010108949f4
Yi Q <yitistica@outlook.com>
2021-07-29 02:52:33 +0800

    iter range

--- src/containers/collections/elementary/sequences.py
+++ src/containers/collections/elementary/sequences.py
@@ -9,8 +9,6 @@ class InvalidCategoryValueError(Exception):
 
 
 class CategorySequence(BaseSequence):
-    """
-    """
     def __init__(self, categories):
 
         super().__init__()
@@ -22,21 +20,40 @@ class CategorySequence(BaseSequence):
 
 class IterView(object):
 
-    def __init__(self, iterable, from_=None, to_=None, step=1, max_loop=1, max_step=None, restart_loop=False):
+    def __init__(self, iterable, from_=None, to_=None, step=1,
+                 max_step=-1, max_cycle=None, max_leap=None, restart=False):
         self._iterable = iterable
-        self._size = None  # subset size;
 
-        self._from, self._to, self._step = self._parse_range(from_=from_, to_=to_, step=step)
-        self._restart = restart_loop
-
-        self._current_step = 0
+        if max_step == -1:
+            max_step = len(self._iterable) - 1
+        elif max_step:
+            assert (isinstance(max_step, int) and max_step >= 0)
         self._max_step = max_step
-        self._max_loop = max_loop
 
-    def _parse_range(self, from_, to_, step):
+        if max_cycle:
+            assert (isinstance(max_cycle, int) and max_cycle >= 0)
+        self._max_cycle = max_cycle
+
+        if max_leap:
+            assert (isinstance(max_leap, int) and max_leap >= 0)
+        self._max_leap = max_leap
+
+        assert isinstance(restart, bool)
+        self._restart = restart
+
+        # setting:
+        self._from, self._to, self._step, self._direction = self._parse_range_args(from_=from_, to_=to_, step=step)
+        self._size = self._to - self._from
+
+        # state to track:
+        self._current_leap = 0
+        self._current_step = 0  # absolute value of steps;
+
+    def _parse_range_args(self, from_, to_, step):
 
         _iterable_size = len(self._iterable)
 
+        # range:
         if from_ is None:
             from_ = 0
         elif 0 <= from_ < _iterable_size:
@@ -45,6 +62,7 @@ class IterView(object):
             from_ = _iterable_size + from_
         else:
             raise IndexError(f"index {from_} out of range")
+        assert isinstance(from_, int)
 
         if to_ is None:
             to_ = _iterable_size
@@ -54,46 +72,79 @@ class IterView(object):
             to_ = _iterable_size + to_
         else:
             raise IndexError(f"index {from_} out of range")
-
-        if step is not None:
-            step = 1
-
-        assert isinstance(from_, int)
         assert isinstance(to_, int)
-        assert isinstance(step, int)
 
         if from_ > to_:
             raise ValueError(f"from_ position is greater than to_ position, "
                              f"use step < 0 to set reverse order.")
-        else:
-            self._size = to_ - from_
 
-        return from_, to_, step
+        # step:
+        direction = 1
+        if step is None:
+            step = 1
+        elif step < 0:
+            direction = -1
+        else:
+            pass
+        step = abs(step)  # only keep track of the magnitude;
+        assert isinstance(step, int)
+
+        return from_, to_, step, direction
+
+    def _convert_step_to_pos(self, steps):
+        """
+        mapping from the number of taken steps to the index in the iterable.
+        # step taken from right to left:
+            if direction == -1: iterable is looped from right to left, the number of steps needed to complete a cycle
+            is unchanged, but pos & index conversion is given as:
+                             ['a', 'b', 'c', 'd']
+            [l-to-r] index:    0,   1,   2,   3  = step
+            [r-to-l] index:    3,   2,   1,   0  = size - step
+        :param steps: int, the number of steps taken from start.
+        :return:
+            cycles: int, the number of times that subsequence of the iterable has been cycled through.
+            cycle_pos: int, the position away from the first item (i.e., step=0) of the subsequence.
+            ltr_pos: int, the position away from the left-most item of the subsequence.
+            index: int, the index of the item that cycle_pos refers to.
+            value: Any, the value of the item by the index.
+        """
+        cycles, cycle_pos = divmod(steps, self._size)  # divmod: Integer = quotient * divisor + remainder
+
+        if self._direction == -1:
+            ltr_pos = self._size - cycle_pos - 1  # to convert r-to-l index back to l-to-r index.
+        else:
+            ltr_pos = cycle_pos
+
+        index = self._from + ltr_pos
+        value = self._iterable[index]
+        return cycles, cycle_pos, ltr_pos, index, value
 
     def __iter__(self):
         return self
 
     def __next__(self):
 
-        if (self._max_step is not None) and (self._current_step >= self._max_step):
+        if (self._max_leap is not None) and (self._current_leap > self._max_leap):
             raise StopIteration
 
-        current_loop, step_in_loop = divmod(self._current_step, self._size)
-
-        if (self._max_loop is not None) and (current_loop >= self._max_loop):
+        if (self._max_step is not None) and (self._current_step > self._max_step):
             raise StopIteration
 
-        if self._restart and (step_in_loop < self._step):
-            step_in_loop = 0
-            increment = (self._step - step_in_loop)
+        cycles, cycle_pos, ltr_pos, index, value = self._convert_step_to_pos(self._current_step)
+
+        if (self._max_cycle is not None) and (cycles > self._max_cycle):
+            raise StopIteration
+
+        # setting current_step for the next candidate step:
+        if self._restart:
+            # can only leap as many as the remaining steps left in the cycle:
+            next_step = min(self._step, self._size - cycle_pos)  # to go back to the 1st pos in the cycle.
         else:
-            increment = self._step
+            next_step = self._step
 
-        self._current_step += increment
+        self._current_step += next_step
 
-        pos = self._from + step_in_loop
-
-        return self._iterable[pos]
+        return value
 
 
 class StatisticsView(object):
@@ -128,7 +179,7 @@ class RandomView(object):
 
 class XList(BaseSequence):
 
-    def range(self, from_=None, to_=None, step=1, max_loop=1, max_step=None, restart_loop=False):
+    def range(self, from_=None, to_=None, step=1, max_step=-1, max_cycle=None, max_leap=None, restart=False):
         return IterView(self._list, from_=from_, to_=to_, step=step,
-                        max_loop=max_loop, max_step=max_step, restart_loop=restart_loop)
+                        max_step=max_step, max_cycle=max_cycle, max_leap=max_leap, restart=restart)
 
