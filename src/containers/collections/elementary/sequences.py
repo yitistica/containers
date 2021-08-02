9aec7a41a0c1d7165996b4eb0025a4003f903843
yitistica <yitistica@outlook.com>
2021-08-02 19:36:45 +0800

    change range view to iter view

--- src/containers/collections/elementary/sequences.py
+++ src/containers/collections/elementary/sequences.py
@@ -34,7 +34,7 @@ class RandomView(object):
 
 class XList(BaseSequence):
 
-    def range(self, from_=None, to_=None, step=1, max_step=-1, max_cycle=None, max_leap=None, restart=False):
+    def iter(self, from_=None, to_=None, step=1, max_step=-1, max_cycle=None, max_leap=None, restart=False):
         return IterView(self._list, from_=from_, to_=to_, step=step,
                         max_step=max_step, max_cycle=max_cycle, max_leap=max_leap, restart=restart)
 
